a18b38c7a3f3a9b270a51cf7fdc62e74b2c99922
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-08-27 08:41:37 +0300

    ✨ (ReminderItem.tsx): Add long press functionality to show actions menu for reminder items.

--- apps/mobile/src/app/reminders/ReminderItem.tsx
+++ apps/mobile/src/app/reminders/ReminderItem.tsx
@@ -1,27 +1,61 @@
 import { ReminderDto } from '@mammimia/types';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import ReminderItemActions from './ReminderItemActions';
 
 type Props = {
   reminder: ReminderDto;
 };
 
 const ReminderItem = ({ reminder }: Props) => {
+  const itemRef = useRef<TouchableOpacity>(null);
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const [anchorPosition, setAnchorPosition] = useState<{
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+  } | null>(null);
+
+  const setVisibility = (visibility: boolean) => {
+    setIsMenuVisible(visibility);
+  };
+
+  const handleLongPress = () => {
+    if (!itemRef.current) {
+      return;
+    }
+
+    itemRef.current.measure((fx, fy, width, height, px, py) => {
+      setAnchorPosition({ x: px + width, y: py + height, width, height });
+      setVisibility(true);
+    });
+  };
+
   return (
-    <TouchableOpacity
-      activeOpacity={0.6}
-      onLongPress={() => {
-        console.log('Long press');
-      }}
-    >
-      <View style={styles.container}>
-        <Text>Title: {reminder.title}</Text>
-        <Text>Content: {reminder.content}</Text>
-        <Text>Due Date: {reminder.expiresAt} </Text>
-        <Text>Folder: {reminder.folder?.name}</Text>
-        <Text>Status: {reminder.status}</Text>
-      </View>
-    </TouchableOpacity>
+    <>
+      <TouchableOpacity
+        ref={itemRef}
+        activeOpacity={0.6}
+        onLongPress={handleLongPress}
+      >
+        <View style={styles.container}>
+          <Text>Title: {reminder.title}</Text>
+          <Text>Content: {reminder.content}</Text>
+          <Text>Due Date: {reminder.expiresAt} </Text>
+          <Text>Folder: {reminder.folder?.name}</Text>
+          <Text>Status: {reminder.status}</Text>
+        </View>
+      </TouchableOpacity>
+      {anchorPosition && (
+        <ReminderItemActions
+          visible={isMenuVisible}
+          closeMenu={() => setVisibility(false)}
+          anchor={anchorPosition}
+          itemStatus={reminder.status}
+        />
+      )}
+    </>
   );
 };
 
