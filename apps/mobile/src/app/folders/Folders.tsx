4a34e5723cc7222251036a49c696a99ae0b74baf
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-08-28 10:28:18 +0300

    ✨ (Folders.tsx): Add a new component Folders to display a list of folders retrieved from the server using AxiosService.

---
+++ apps/mobile/src/app/folders/Folders.tsx
@@ -0,0 +1,38 @@
+import { FolderDto } from '@mammimia/types';
+import React, { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import AxiosService from '../services/AxiosService';
+import FolderSliderItem from './FolderSliderItem';
+
+const Folders = () => {
+  const [folders, setFolders] = useState<FolderDto[]>([]);
+
+  useEffect(() => {
+    AxiosService.get<FolderDto[]>('folders')
+      .then((response) => setFolders(response.data))
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Folders</Text>
+      <FlatList
+        data={folders}
+        renderItem={({ item }) => <FolderSliderItem folder={item} />}
+        keyExtractor={(item) => item.id}
+        horizontal
+      />
+    </View>
+  );
+};
+
+export default Folders;
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 10,
+    padding: 10,
+  },
+});
