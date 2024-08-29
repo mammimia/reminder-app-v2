3c92195cc06aab8b3a019a31c915933811dc8d1c
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-08-29 08:15:22 +0300

    ✨ (ReminderEditorModal.tsx): Add a new ReminderEditorModal component for creating and editing reminders in the mobile app.

---
+++ apps/mobile/src/app/reminders/ReminderEditorModal.tsx
@@ -0,0 +1,65 @@
+import { CreateReminderDto } from '@mammimia/types';
+import { Formik } from 'formik';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button, Modal, Text, TextInput } from 'react-native-paper';
+
+type Props = {
+  defaultValues?: CreateReminderDto;
+  visible: boolean;
+  hideModal: () => void;
+};
+
+const ReminderEditorModal = ({ defaultValues, visible, hideModal }: Props) => {
+  const containerStyle = { backgroundColor: 'white', padding: 20 };
+
+  const handleSubmit = (values: CreateReminderDto) => {
+    const parsedValues = CreateReminderDto.parse(values);
+    console.log(parsedValues);
+  };
+
+  return (
+    <Modal
+      visible={visible}
+      onDismiss={hideModal}
+      contentContainerStyle={containerStyle}
+    >
+      <Text style={styles.formTitle}>Reminder Editor</Text>
+      <Formik
+        initialValues={defaultValues || { title: '', content: '' }}
+        onSubmit={handleSubmit}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+          <View style={styles.formContainer}>
+            <TextInput
+              label="Title"
+              onChangeText={handleChange('title')}
+              onBlur={handleBlur('title')}
+              value={values.title}
+            />
+            <TextInput
+              label="Content"
+              onChangeText={handleChange('content')}
+              onBlur={handleBlur('content')}
+              value={values.content}
+            />
+            <Button onPress={handleSubmit}>Submit</Button>
+          </View>
+        )}
+      </Formik>
+    </Modal>
+  );
+};
+
+export default ReminderEditorModal;
+
+const styles = StyleSheet.create({
+  formTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  formContainer: {
+    gap: 10,
+  },
+});
