aea136fdbf92473fda9445d1e0378641a6839d09
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-09-02 13:29:33 +0300

    ✨ (App.tsx): Add createDrawerNavigator to create a new drawer navigation for reminders ⬆️ (package.json): Upgrade @react-navigation/drawer to version 6.7.2 ⬆️ (package.json): Upgrade react-native-reanimated to version 3.10.1

--- apps/mobile/src/app/App.tsx
+++ apps/mobile/src/app/App.tsx
@@ -1,12 +1,23 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 import Folders from './folders/Folders';
-import Reminders from './reminders/Reminders';
 import RemindersHome from './reminders/RemindersHome';
+
+const ReminderDrawer = createDrawerNavigator();
+
+function ReminderStackScreen() {
+  return (
+    <ReminderDrawer.Navigator initialRouteName="Home">
+      <ReminderDrawer.Screen name="Home" component={RemindersHome} />
+      <ReminderDrawer.Screen name="Details" component={RemindersHome} />
+    </ReminderDrawer.Navigator>
+  );
+}
 
 const Tab = createBottomTabNavigator();
 
@@ -15,8 +26,8 @@ export const App = () => {
     <NavigationContainer>
       <PaperProvider>
         <StatusBar barStyle="dark-content" />
-        <Tab.Navigator>
-          <Tab.Screen name="Reminders" component={RemindersHome} />
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
+          <Tab.Screen name="ReminderStack" component={ReminderStackScreen} />
           <Tab.Screen name="Folders" component={Folders} />
         </Tab.Navigator>
       </PaperProvider>
