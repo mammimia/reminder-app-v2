5dd982a08397db874efbaebf2fe3092e9ef17c80
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-09-08 07:29:18 +0300

    🔧 (CategoriesList.tsx): refactor CategoriesList component to use custom hook useFetchData for fetching categories data and improve code readability

--- apps/mobile/src/app/ categories/CategoriesList.tsx
+++ apps/mobile/src/app/ categories/CategoriesList.tsx
@@ -1,36 +1,22 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
-import React, { useEffect, useState } from 'react';
 import { CategoryDto } from '@mammimia/types';
+import React from 'react';
+import { FlatList, StyleSheet, View } from 'react-native';
+import useFetchData from '../../hooks/useFetchData';
 import CategoryService from '../services/CategoryService';
 import CategorySliderItem from './CategorySliderItem';
 
 const CategoriesList = () => {
-  const [categories, setFolders] = useState<CategoryDto[]>([]);
-  const [isFetching, setIsFetching] = useState<boolean>(false);
-
-  const getCategories = async () => {
-    setIsFetching(true);
-    try {
-      const response = await CategoryService.get();
-      setFolders(response.data);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsFetching(false);
-    }
-  };
-
-  useEffect(() => {
-    getCategories();
-  }, []);
+  const { data, isFetching, refetch } = useFetchData<CategoryDto>({
+    fetchMethod: CategoryService.get,
+  });
 
   return (
     <View style={styles.container}>
       <FlatList
-        data={categories}
+        data={data}
         renderItem={({ item }) => <CategorySliderItem category={item} />}
         keyExtractor={(item) => item.id}
-        onRefresh={getCategories}
+        onRefresh={refetch}
         refreshing={isFetching}
       />
     </View>
