d70addf448d65ed7389444ae98cb4c788b825e74
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-09-08 07:35:35 +0300

    🔧 (FolderList.tsx): refactor FolderList component to use custom hook useFetchData for fetching folders data and improve code readability

--- apps/mobile/src/app/folders/FolderList.tsx
+++ apps/mobile/src/app/folders/FolderList.tsx
@@ -1,6 +1,7 @@
 import { FolderDto } from '@mammimia/types';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
+import useFetchData from '../../hooks/useFetchData';
 import FolderService from '../services/FolderService';
 import FolderSliderItem from './FolderSliderItem';
 
@@ -15,37 +16,22 @@ const FolderList = ({
   flatListStyle,
   folderItemStyle,
 }: Props) => {
-  const [folders, setFolders] = useState<FolderDto[]>([]);
-  const [isFetching, setIsFetching] = useState<boolean>(false);
-
-  const getFolders = async () => {
-    setIsFetching(true);
-    try {
-      const response = await FolderService.get();
-      setFolders(response.data);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsFetching(false);
-    }
-  };
-
-  useEffect(() => {
-    getFolders();
-  }, []);
+  const { data, isFetching, refetch } = useFetchData<FolderDto>({
+    fetchMethod: FolderService.get,
+  });
 
   return (
     <View style={styles.container}>
       <Text>Folders</Text>
       <FlatList
         style={flatListStyle}
-        data={folders}
+        data={data}
         renderItem={({ item }) => (
           <FolderSliderItem folder={item} style={folderItemStyle} />
         )}
         keyExtractor={(item) => item.id}
         horizontal={horizontal}
-        onRefresh={getFolders}
+        onRefresh={refetch}
         refreshing={isFetching}
       />
     </View>
