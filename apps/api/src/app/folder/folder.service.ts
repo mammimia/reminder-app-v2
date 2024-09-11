9420691c53424070e0303c69abdd522ff7da9613
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-09-11 08:58:41 +0300

    ✨ (folder.service.ts): add orderBy field to query folders by reminders count in descending order

--- apps/api/src/app/folder/folder.service.ts
+++ apps/api/src/app/folder/folder.service.ts
@@ -18,6 +18,11 @@ export class FolderService {
           },
         },
       },
+      orderBy: {
+        reminders: {
+          _count: 'desc',
+        },
+      },
     });
 
     return folders;
