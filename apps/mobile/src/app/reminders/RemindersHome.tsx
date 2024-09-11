99da28a80db1f00773d5805270ace31d4c2d1b6e
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-09-11 08:58:26 +0300

    ✨ (RemindersHome.tsx): Introduce custom styles using the `useStyles` hook from '@mammimia/ui' for better theming and consistency 📝 (RemindersHome.tsx): Add fetching logic for folders using `useFetchData` hook and `FolderService.get` method to populate the FolderList component 🔧 (RemindersHome.tsx): Refactor styles into a separate function `createStyles` for better readability and maintainability

--- apps/mobile/src/app/reminders/RemindersHome.tsx
+++ apps/mobile/src/app/reminders/RemindersHome.tsx
@@ -1,12 +1,30 @@
+import { TColors, useStyles } from '@mammimia/ui';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import FolderList from '../folders/FolderList';
 import Reminders from './Reminders';
+import useFetchData from '../../hooks/useFetchData';
+import { FolderDto } from '@mammimia/types';
+import FolderService from '../services/FolderService';
 
 const RemindersHome = () => {
+  const { colors, styles } = useStyles(createStyles);
+  const {
+    data: folders,
+    isFetching,
+    refetch,
+  } = useFetchData<FolderDto>({
+    fetchMethod: FolderService.get,
+  });
+
   return (
     <View style={styles.container}>
-      <FolderList folderItemStyle={styles.folderItemStyle} />
+      <FolderList
+        data={folders}
+        isFetching={isFetching}
+        onRefresh={refetch}
+        folderItemStyle={styles.folderItemStyle}
+      />
       <Reminders />
     </View>
   );
@@ -14,11 +32,13 @@ const RemindersHome = () => {
 
 export default RemindersHome;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  folderItemStyle: {
-    width: 150,
-  },
-});
+const createStyles = (colors: TColors) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.background,
+    },
+    folderItemStyle: {
+      width: 150,
+    },
+  });
