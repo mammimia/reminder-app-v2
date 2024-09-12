a8822f25545f9682fa024d450c210dec44a7da49
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-09-12 09:12:59 +0300

    ✨ (App.tsx): Introduce ActionSheetProvider from '@expo/react-native-action-sheet' to enable action sheet functionality in the app 🔧 (package.json): Add "@expo/react-native-action-sheet" dependency to enable action sheet functionality in the app

--- apps/mobile/src/app/App.tsx
+++ apps/mobile/src/app/App.tsx
@@ -1,18 +1,19 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
+import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 import { Ionicons } from '@expo/vector-icons';
+import { ThemeProvider } from '@mammimia/ui';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
-import FolderList from './folders/FolderList';
-import RemindersHome from './reminders/RemindersHome';
-import FolderDetails from './folders/FolderDetails';
-import Folders from './folders/Folders';
 import Categories from './ categories/Categories';
-import { ThemeProvider } from '@mammimia/ui';
+import FolderDetails from './folders/FolderDetails';
+import FolderList from './folders/FolderList';
+import Folders from './folders/Folders';
+import RemindersHome from './reminders/RemindersHome';
 
 const ReminderStack = createStackNavigator();
 
@@ -46,46 +47,48 @@ export const App = () => {
   return (
     <NavigationContainer>
       <PaperProvider>
-        <ThemeProvider>
-          <StatusBar barStyle="dark-content" />
-          <Tab.Navigator screenOptions={{ headerShown: false }}>
-            <Tab.Screen
-              name="Reminder"
-              options={{
-                tabBarIcon: ({ focused, color, size }) => (
-                  <Ionicons
-                    name={!focused ? 'fish' : 'fish-outline'}
-                    color={color}
-                    size={size}
-                  />
-                ),
-              }}
-              component={ReminderDrawerScreen}
-            />
-            <Tab.Screen
-              name="Expense"
-              options={{
-                tabBarIcon: ({ color, size }) => (
-                  <Ionicons name="cash-outline" color={color} size={size} />
-                ),
-              }}
-              component={FolderList}
-            />
-            <Tab.Screen
-              name="Daily Routine"
-              options={{
-                tabBarIcon: ({ focused, color, size }) => (
-                  <Ionicons
-                    name={!focused ? 'calendar' : 'calendar-outline'}
-                    color={color}
-                    size={size}
-                  />
-                ),
-              }}
-              component={FolderList}
-            />
-          </Tab.Navigator>
-        </ThemeProvider>
+        <ActionSheetProvider>
+          <ThemeProvider>
+            <StatusBar barStyle="dark-content" />
+            <Tab.Navigator screenOptions={{ headerShown: false }}>
+              <Tab.Screen
+                name="Reminder"
+                options={{
+                  tabBarIcon: ({ focused, color, size }) => (
+                    <Ionicons
+                      name={!focused ? 'fish' : 'fish-outline'}
+                      color={color}
+                      size={size}
+                    />
+                  ),
+                }}
+                component={ReminderDrawerScreen}
+              />
+              <Tab.Screen
+                name="Expense"
+                options={{
+                  tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="cash-outline" color={color} size={size} />
+                  ),
+                }}
+                component={FolderList}
+              />
+              <Tab.Screen
+                name="Daily Routine"
+                options={{
+                  tabBarIcon: ({ focused, color, size }) => (
+                    <Ionicons
+                      name={!focused ? 'calendar' : 'calendar-outline'}
+                      color={color}
+                      size={size}
+                    />
+                  ),
+                }}
+                component={FolderList}
+              />
+            </Tab.Navigator>
+          </ThemeProvider>
+        </ActionSheetProvider>
       </PaperProvider>
     </NavigationContainer>
   );
