3fd83daa9aa285c1432c1df0332fb0bd57309031
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-09-17 09:55:38 +0300

    ✨ (FolderSliderItem.tsx): Introduce new styles and structure for FolderSliderItem component to improve readability and maintainability. Add support for custom colors and styling using the useStyles hook from @mammimia/ui.

--- apps/mobile/src/app/folders/FolderSliderItem.tsx
+++ apps/mobile/src/app/folders/FolderSliderItem.tsx
@@ -1,12 +1,13 @@
+import { useActionSheet } from '@expo/react-native-action-sheet';
 import { FolderDto } from '@mammimia/types';
+import { TColors, useStyles } from '@mammimia/ui';
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import showCustomActionSheet, {
   ActionSheetOption,
 } from '../../utils/showCustomActionSheet';
 import FolderService from '../services/FolderService';
-import { useActionSheet } from '@expo/react-native-action-sheet';
 
 type Props = {
   folder: FolderDto;
@@ -25,6 +26,7 @@ const FolderSliderItem = ({
 }: Props) => {
   const navigation = useNavigation();
   const { showActionSheetWithOptions } = useActionSheet();
+  const { styles } = useStyles(createStyles);
 
   const handleDelete = () => {
     FolderService.remove(folder.id)
@@ -66,7 +68,7 @@ const FolderSliderItem = ({
 
   return (
     <TouchableOpacity
-      style={[styles.container, { backgroundColor: folder.color }, style]}
+      style={[styles.container, style]}
       activeOpacity={0.6}
       onPress={() => {
         navigation.navigate('FolderDetails', {
@@ -76,52 +78,68 @@ const FolderSliderItem = ({
       }}
       onLongPress={handleLongPress}
     >
-      <Text style={styles.folderName}>{folder.name}</Text>
-      <Text style={styles.reminderCount}>
-        {folder.reminderCount || 0} reminders
-      </Text>
-      <Text style={styles.categoryText}>
-        {folder.category?.name || 'General'}
-      </Text>
+      <View
+        style={[
+          styles.folderColorBar,
+          {
+            backgroundColor: folder.color || 'transparent',
+          },
+        ]}
+      />
+      <View style={styles.innerContainer}>
+        <Text style={styles.folderName}>{folder.name}</Text>
+        <Text style={styles.reminderCount}>
+          {folder.reminderCount || 0} reminders
+        </Text>
+        <Text style={styles.categoryText}>
+          {folder.category?.name || 'General'}
+        </Text>
+      </View>
     </TouchableOpacity>
   );
 };
 
 export default FolderSliderItem;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'gray',
-    padding: 10,
-    marginBottom: 10,
-    marginHorizontal: 10,
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: 'black',
-    borderRadius: 10,
-    height: 100,
-  },
-  folderName: {
-    fontSize: 14,
-    color: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  reminderCount: {
-    fontSize: 12,
-    color: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: 5,
-  },
-  categoryText: {
-    fontSize: 12,
-    color: 'white',
-    position: 'absolute',
-    bottom: 0,
-    right: 0,
-    padding: 5,
-  },
-});
+const createStyles = (colors: TColors) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: colors.gray300,
+      marginBottom: 10,
+      marginHorizontal: 10,
+      borderWidth: 1,
+      borderStyle: 'solid',
+      borderColor: colors.darkblue900,
+      height: 100,
+    },
+    innerContainer: {
+      flex: 1,
+      flexDirection: 'column',
+      padding: 10,
+    },
+    folderColorBar: {
+      height: 5,
+    },
+    folderName: {
+      fontSize: 14,
+      fontWeight: 'bold',
+      color: colors.darkblue900,
+    },
+    reminderCount: {
+      fontSize: 12,
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingTop: 5,
+      color: colors.darkblue900,
+    },
+    categoryText: {
+      fontSize: 12,
+      position: 'absolute',
+      bottom: 0,
+      right: 0,
+      padding: 5,
+      color: colors.darkblue900,
+    },
+  });
