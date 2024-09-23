69ef1e0977077dcc5411feac1955a4b4be6857d9
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-09-23 11:44:57 +0300

    ✨ (ReminderCalendar.tsx): Refactor ReminderCalendar component to improve UI and functionality by adding a flat list for displaying days of the week and updating styles for better user experience.

--- apps/mobile/src/app/reminders/ReminderCalendar.tsx
+++ apps/mobile/src/app/reminders/ReminderCalendar.tsx
@@ -1,33 +1,41 @@
 import { Ionicons } from '@expo/vector-icons';
 import { TColors, useStyles } from '@mammimia/ui';
 import React, { useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import DateUtils from '../../utils/DateUtils';
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import DateTimePicker from '../../components/DateTimePicker';
+import DateUtils from '../../utils/DateUtils';
 
 const ReminderCalendar = () => {
-  const [date, setDate] = useState<string | undefined>(
-    new Date().toISOString()
-  );
+  const [date, setDate] = useState<string>(new Date().toISOString());
 
   const { styles, colors } = useStyles(createStyles);
+
+  const week = DateUtils.getWeek(date);
+
+  const handleDayPress = (day: string) => {
+    setDate(day);
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.headerContainer}>
         <View>
-          <Text style={styles.header}>{DateUtils.formatDate(date)}</Text>
+          <Text style={styles.header}>
+            {DateUtils.formatDate(date, 'dd MMM yyyy, EEEE')}
+          </Text>
           <Text style={styles.infoText}>10 tasks</Text>
         </View>
         <DateTimePicker
           value={date}
           onChange={(d) => setDate(d)}
           datePickerStyle={{
-            position: 'absolute',
-            top: 50,
-            left: 0,
-            right: 0,
-            backgroundColor: colors.white,
+            flex: 1,
           }}
         >
           {({ onPress }) => (
@@ -37,6 +45,38 @@ const ReminderCalendar = () => {
           )}
         </DateTimePicker>
       </View>
+
+      <FlatList
+        contentContainerStyle={styles.dayContainer}
+        data={week}
+        keyExtractor={(item) => item.date}
+        renderItem={({ item }) => {
+          const isSameDay = DateUtils.isSameDay(date, item.date);
+          return (
+            <TouchableOpacity onPress={() => handleDayPress(item.date)}>
+              <View style={[styles.dayItem, isSameDay && styles.selectedDay]}>
+                <Text
+                  style={[
+                    styles.dayItemText,
+                    isSameDay && styles.selectedDayText,
+                  ]}
+                >
+                  {item.dayOfWeek}
+                </Text>
+                <Text
+                  style={[
+                    styles.dayItemText,
+                    isSameDay && styles.selectedDayText,
+                  ]}
+                >
+                  {DateUtils.formatDate(item.date, 'dd')}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          );
+        }}
+        horizontal
+      />
     </View>
   );
 };
@@ -55,6 +95,7 @@ const createStyles = (colors: TColors) =>
       justifyContent: 'space-between',
       alignItems: 'center',
       position: 'relative',
+      marginBottom: 20,
     },
     header: {
       fontSize: 24,
@@ -68,5 +109,30 @@ const createStyles = (colors: TColors) =>
       backgroundColor: colors.primary,
       padding: 10,
       borderRadius: 50,
+    },
+    dayContainer: {
+      flex: 1,
+      justifyContent: 'space-between',
+      marginHorizontal: 10,
+      height: 50,
+    },
+    dayItem: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: 45,
+      padding: 5,
+    },
+    dayItemText: {
+      color: colors.text,
+      opacity: 0.6,
+    },
+    selectedDay: {
+      backgroundColor: colors.primary,
+      borderRadius: 50,
+    },
+    selectedDayText: {
+      color: colors.white,
+      fontWeight: 'bold',
+      opacity: 1,
     },
   });
