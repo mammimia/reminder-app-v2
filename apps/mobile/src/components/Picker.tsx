ba07e5fb2dff5841a0ab64ff7524d149e96082a6
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-09-24 09:47:35 +0300

    ✨ (Picker.tsx): Add a new Picker component to handle selection of items with different behaviors for iOS and Android platforms.

---
+++ apps/mobile/src/components/Picker.tsx
@@ -0,0 +1,139 @@
+import { TColors, useStyles } from '@mammimia/ui';
+import { Picker as RNPicker } from '@react-native-picker/picker';
+
+import React, { ChangeEvent, useState } from 'react';
+import { Platform, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Button, Portal, Text, TextInput } from 'react-native-paper';
+
+type PickerItem = {
+  label: string;
+  value: string;
+};
+
+type Props = {
+  handleChange: (e: string | ChangeEvent<any> | null) => void;
+  items: PickerItem[];
+  value: string | undefined;
+};
+
+const Picker = ({ value, items, handleChange }: Props) => {
+  const { styles } = useStyles(createStyles);
+
+  if (Platform.OS === 'ios') {
+    const selectedValue = items.find((item) => item.value === value);
+    return (
+      <IOSPicker
+        value={selectedValue?.label || ''}
+        items={items}
+        handleChange={handleChange}
+      />
+    );
+  }
+
+  return (
+    <RNPicker
+      style={styles.androidPickerContainer}
+      selectedValue={value}
+      onValueChange={(itemValue) => handleChange(itemValue)}
+    >
+      {items.map((item) => (
+        <RNPicker.Item key={item.value} label={item.label} value={item.value} />
+      ))}
+    </RNPicker>
+  );
+};
+
+type IOSPickerProps = {
+  value: string;
+  items: PickerItem[];
+  handleChange: (value: string | null) => void;
+};
+
+const IOSPicker = ({ value, items, handleChange }: IOSPickerProps) => {
+  const { styles } = useStyles(createStyles);
+  const [visible, setVisible] = useState(false);
+  const [selectedValue, setSelectedValue] = useState(null);
+
+  const handleCancel = () => {
+    setVisible(false);
+  };
+
+  const handleDone = () => {
+    setVisible(false);
+    handleChange(selectedValue);
+  };
+
+  return (
+    <>
+      <TextInput
+        label="Folder"
+        editable={false}
+        onPress={() => setVisible(true)}
+        value={value}
+      />
+      {visible && (
+        <Portal>
+          <View
+            style={{
+              width: '100%',
+              position: 'absolute',
+              bottom: 0,
+              backgroundColor: 'white',
+            }}
+          >
+            <RNPicker
+              selectedValue={selectedValue}
+              onValueChange={(value) => setSelectedValue(value)}
+            >
+              {items.map((item) => (
+                <RNPicker.Item
+                  key={item.value}
+                  label={item.label}
+                  value={item.value}
+                />
+              ))}
+            </RNPicker>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity onPress={handleDone}>
+                <Button style={[styles.button]}>
+                  <Text style={styles.successButtonText}>Done</Text>
+                </Button>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={handleCancel}>
+                <Button style={[styles.button]}>
+                  <Text style={styles.cancelButtonText}>Cancel</Text>
+                </Button>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Portal>
+      )}
+    </>
+  );
+};
+
+export default Picker;
+
+const createStyles = (colors: TColors) =>
+  StyleSheet.create({
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+    },
+    button: {
+      padding: 5,
+      borderRadius: 50,
+      margin: 5,
+    },
+    successButtonText: {
+      color: colors.text,
+    },
+    cancelButtonText: {
+      color: colors.text,
+    },
+    androidPickerContainer: {
+      width: '100%',
+      backgroundColor: colors.red100, // TODO: Need a proper color
+    },
+  });
