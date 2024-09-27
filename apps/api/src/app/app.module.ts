8e741e377afdc72ffc8e1a30832c75a42a78c3a6
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-09-27 12:36:09 +0300

    ✨ (app.module.ts): add PaymentModule to the list of imports in the AppModule to integrate payment functionality into the application ✨ (payment.module.ts): create PaymentModule to handle payment-related features and services in the application

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { ReminderModule } from './reminder/reminder.module';
 import { FolderModule } from './folder/folder.module';
 import { CategoryModule } from './category/category.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [ReminderModule, FolderModule, CategoryModule],
+  imports: [ReminderModule, FolderModule, CategoryModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService],
 })
