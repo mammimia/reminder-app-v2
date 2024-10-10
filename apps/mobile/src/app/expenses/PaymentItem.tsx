27e59509edd5ef448c731e4c3761c13ea1f69b4b
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-10-10 09:00:13 +0300

    ✨ (PaymentItem.tsx): Create a new component PaymentItem to display payment details in a list format ♻️ (Payments.tsx): Refactor Payments component to use the newly created PaymentItem component for rendering payment items in a FlatList, improving code readability and reusability. Remove redundant styling code from Payments component.

---
+++ apps/mobile/src/app/expenses/PaymentItem.tsx
@@ -0,0 +1,72 @@
+import { PaymentDto, TransactionType } from '@mammimia/types';
+import { TColors, useStyles } from '@mammimia/ui';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import AmountUtils from '../../utils/AmountUtils';
+import DateUtils from '../../utils/DateUtils';
+
+type Props = {
+  item: PaymentDto;
+};
+
+const PaymentItem = ({ item }: Props) => {
+  const { styles, colors } = useStyles(createStyles);
+  return (
+    <View style={styles.paymentItem}>
+      <View>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.date}>
+          {DateUtils.formatDate(item.paymentDate)}
+        </Text>
+      </View>
+      <View style={styles.amountContainer}>
+        <Text
+          style={[
+            styles.amount,
+            item.transactionType === TransactionType.INCOME
+              ? { color: colors.green500 }
+              : { color: colors.red500 },
+          ]}
+        >
+          {item.transactionType === TransactionType.INCOME ? ' + ' : ' - '}
+          {AmountUtils.format(item.amount, item.currency)}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default PaymentItem;
+
+const createStyles = (colors: TColors) =>
+  StyleSheet.create({
+    paymentItem: {
+      padding: 10,
+      marginHorizontal: 20,
+      marginVertical: 5,
+      borderBottomWidth: 1,
+      borderBottomColor: colors.gray500,
+      justifyContent: 'space-between',
+      flexDirection: 'row',
+    },
+    date: {
+      fontSize: 12,
+    },
+    title: {
+      fontSize: 14,
+      fontWeight: 'bold',
+    },
+    description: {
+      fontSize: 12,
+      color: colors.gray900,
+    },
+    amountContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    amount: {
+      textAlign: 'right',
+      fontSize: 15,
+      fontWeight: 'bold',
+    },
+  });
--- apps/mobile/src/app/expenses/Payments.tsx
+++ apps/mobile/src/app/expenses/Payments.tsx
@@ -1,14 +1,11 @@
-import { PaymentDto, TransactionType } from '@mammimia/types';
-import { TColors, useStyles } from '@mammimia/ui';
+import { PaymentDto } from '@mammimia/types';
 import React from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList } from 'react-native';
 import useFetchData from '../../hooks/useFetchData';
-import AmountUtils from '../../utils/AmountUtils';
-import DateUtils from '../../utils/DateUtils';
 import PaymentService from '../services/PaymentService';
+import PaymentItem from './PaymentItem';
 
 const Payments = () => {
-  const { styles, colors } = useStyles(createStyles);
   const { data } = useFetchData<PaymentDto>({
     fetchMethod: PaymentService.get,
   });
@@ -16,65 +13,10 @@ const Payments = () => {
   return (
     <FlatList
       data={data}
-      renderItem={({ item }) => (
-        <View style={styles.paymentItem}>
-          <View>
-            <Text style={styles.title}>{item.title}</Text>
-            <Text style={styles.date}>
-              {DateUtils.formatDate(item.paymentDate)}
-            </Text>
-          </View>
-          <View style={styles.amountContainer}>
-            <Text
-              style={[
-                styles.amount,
-                item.transactionType === TransactionType.INCOME
-                  ? { color: colors.green500 }
-                  : { color: colors.red500 },
-              ]}
-            >
-              {item.transactionType === TransactionType.INCOME ? ' + ' : ' - '}
-              {AmountUtils.format(item.amount, item.currency)}
-            </Text>
-          </View>
-        </View>
-      )}
+      renderItem={({ item }) => <PaymentItem item={item} />}
       keyExtractor={(item) => item.id}
     />
   );
 };
 
 export default Payments;
-
-const createStyles = (colors: TColors) =>
-  StyleSheet.create({
-    paymentItem: {
-      padding: 10,
-      marginHorizontal: 20,
-      marginVertical: 5,
-      borderBottomWidth: 1,
-      borderBottomColor: colors.gray500,
-      justifyContent: 'space-between',
-      flexDirection: 'row',
-    },
-    date: {
-      fontSize: 12,
-    },
-    title: {
-      fontSize: 14,
-      fontWeight: 'bold',
-    },
-    description: {
-      fontSize: 12,
-      color: colors.gray900,
-    },
-    amountContainer: {
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    amount: {
-      textAlign: 'right',
-      fontSize: 15,
-      fontWeight: 'bold',
-    },
-  });
