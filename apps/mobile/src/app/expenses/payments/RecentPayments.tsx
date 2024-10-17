7c3ed6ef611889997092165fb4f0f93e4c59b446
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-10-17 09:33:05 +0300

    ✨ (RecentPayments.tsx): introduce useFetchData hook to fetch recent payments data with a limit of 5 📝 (RecentPayments.tsx): import PaymentDto type and PaymentService to handle payment data and services 🔧 (RecentPayments.tsx): add PaymentList component to display fetched payments with loading indicator and refresh functionality

--- apps/mobile/src/app/expenses/payments/RecentPayments.tsx
+++ apps/mobile/src/app/expenses/payments/RecentPayments.tsx
@@ -1,11 +1,19 @@
+import { PaymentDto } from '@mammimia/types';
 import { ParamListBase, useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import SectionTitle from '../../../components/SectionTitle';
+import useFetchData from '../../../hooks/useFetchData';
+import PaymentService from '../../services/PaymentService';
+import PaymentList from './PaymentList';
 
 const RecentPayments = () => {
   const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
+  const { data, isFetching, refetch } = useFetchData<PaymentDto>({
+    fetchMethod: PaymentService.get,
+    params: { limit: 5 },
+  });
 
   return (
     <View style={styles.container}>
@@ -14,6 +22,11 @@ const RecentPayments = () => {
         handleSeeAll={() => {
           navigation.navigate('Payments');
         }}
+      />
+      <PaymentList
+        payments={data}
+        isFetching={isFetching}
+        onRefresh={refetch}
       />
     </View>
   );
