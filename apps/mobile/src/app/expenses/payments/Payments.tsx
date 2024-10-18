6c952e9e7f312394d8c986fef7805ebd9ffba3bb
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-10-18 12:02:12 +0300

    ✨ (Payments.tsx): introduce PaymentEditorModal component to handle editing payments 🔧 (Payments.tsx): add styles to container in Payments component for better layout and spacing

--- apps/mobile/src/app/expenses/payments/Payments.tsx
+++ apps/mobile/src/app/expenses/payments/Payments.tsx
@@ -1,7 +1,11 @@
 import { PaymentDto } from '@mammimia/types';
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import AddFab from '../../../components/AddFab';
+import useEditorModal from '../../../hooks/useEditorModal';
 import useFetchData from '../../../hooks/useFetchData';
 import PaymentService from '../../services/PaymentService';
+import PaymentEditorModal from './PaymentEditorModal';
 import PaymentList from './PaymentList';
 
 const Payments = () => {
@@ -9,9 +13,35 @@ const Payments = () => {
     fetchMethod: PaymentService.get,
   });
 
+  const { modalVisible, selectedItem, hideModal, openModal } =
+    useEditorModal<PaymentDto>();
+
   return (
-    <PaymentList payments={data} isFetching={isFetching} onRefresh={refetch} />
+    <>
+      <View style={styles.container}>
+        <PaymentList
+          payments={data}
+          isFetching={isFetching}
+          onRefresh={refetch}
+        />
+        <AddFab onPress={() => openModal()} />
+      </View>
+      <PaymentEditorModal
+        visible={modalVisible}
+        hideModal={hideModal}
+        defaultValues={selectedItem}
+        refetch={refetch}
+      />
+    </>
   );
 };
 
 export default Payments;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 10,
+    padding: 10,
+  },
+});
