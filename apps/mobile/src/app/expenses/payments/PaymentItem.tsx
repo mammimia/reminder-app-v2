f3892d2c19d042e273f5917d2577240c6bd640f9
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-10-22 08:32:41 +0300

    ✨ (PaymentItem.tsx): Add long press functionality to PaymentItem component to show custom action sheet with options for editing, deleting, or closing a payment. Add support for making the component pressable based on a boolean prop.

--- apps/mobile/src/app/expenses/payments/PaymentItem.tsx
+++ apps/mobile/src/app/expenses/payments/PaymentItem.tsx
@@ -1,49 +1,93 @@
+import { useActionSheet } from '@expo/react-native-action-sheet';
 import { PaymentDto, TransactionType } from '@mammimia/types';
 import { TColors, useStyles } from '@mammimia/ui';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import ColorCircle from '../../../components/ColorCircle';
 import AmountUtils from '../../../utils/AmountUtils';
 import DateUtils from '../../../utils/DateUtils';
 import StringUtils from '../../../utils/StringUtils';
+import showCustomActionSheet, {
+  ActionSheetOption,
+} from '../../../utils/showCustomActionSheet';
 
 type Props = {
   item: PaymentDto;
+  pressable?: boolean;
 };
 
-const PaymentItem = ({ item }: Props) => {
+const PaymentItem = ({ item, pressable }: Props) => {
   const { styles, colors } = useStyles(createStyles);
+  const { showActionSheetWithOptions } = useActionSheet();
+
+  const handleLongPress = () => {
+    if (!pressable) return;
+
+    const options: ActionSheetOption[] = [
+      {
+        label: 'Edit',
+        onPress: () => console.log('Edit'),
+      },
+      {
+        label: 'Delete',
+        onPress: () => console.log('Delete'),
+        isDestructive: true,
+      },
+      {
+        label: 'Close',
+        onPress: () => {
+          /* Do nothing */
+        },
+        isCancel: true,
+      },
+    ];
+
+    showCustomActionSheet(
+      {
+        options,
+        title: 'Payment Actions',
+        message: 'Select an option to manage your payment.',
+        tintColor: colors.primary,
+      },
+      showActionSheetWithOptions
+    );
+  };
 
   return (
-    <View style={styles.paymentItem}>
-      <View style={styles.innerContainer}>
-        <Text style={styles.title}>{item.title}</Text>
-        <Text style={styles.date}>
-          {DateUtils.formatDate(item.paymentDate)}
-        </Text>
+    <TouchableOpacity
+      activeOpacity={pressable ? 0.2 : 1}
+      onLongPress={handleLongPress}
+    >
+      <View style={styles.paymentItem}>
+        <View style={styles.innerContainer}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.date}>
+            {DateUtils.formatDate(item.paymentDate)}
+          </Text>
+        </View>
+        <View style={styles.amountContainer}>
+          <Text
+            style={[
+              styles.amount,
+              item.transactionType === TransactionType.INCOME
+                ? { color: colors.green500 }
+                : { color: colors.red500 },
+            ]}
+          >
+            {item.transactionType === TransactionType.INCOME ? ' + ' : ' - '}
+            {AmountUtils.format(item.amount, item.currency)}
+          </Text>
+          {item.type && (
+            <View style={styles.paymentType}>
+              <ColorCircle color={item.type.color} size={12} />
+              <Text style={styles.description}>
+                {StringUtils.convertToTitleCase(item.type.name)}
+              </Text>
+            </View>
+          )}
+        </View>
       </View>
-      <View style={styles.amountContainer}>
-        <Text
-          style={[
-            styles.amount,
-            item.transactionType === TransactionType.INCOME
-              ? { color: colors.green500 }
-              : { color: colors.red500 },
-          ]}
-        >
-          {item.transactionType === TransactionType.INCOME ? ' + ' : ' - '}
-          {AmountUtils.format(item.amount, item.currency)}
-        </Text>
-        {item.type && (
-          <View style={styles.paymentType}>
-            <ColorCircle color={item.type.color} size={12} />
-            <Text style={styles.description}>
-              {StringUtils.convertToTitleCase(item.type.name)}
-            </Text>
-          </View>
-        )}
-      </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
